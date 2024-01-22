19e6dbdabcf6d2cf33347f14a8a510aaee6a23db
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-22 12:44:04 +0800

    Initial CMake cxx17 projects.

---
+++ inc/nava/core/repoplugins/utils.h
@@ -0,0 +1,20 @@
+#ifndef NAVA_CORE_REPO_PLUGINS_V2_UTILS_H
+#define NAVA_CORE_REPO_PLUGINS_V2_UTILS_H
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace repoplugins {
+  
+class Utils {
+ private:
+ public:
+  Utils();
+  ~Utils();
+};
+
+}  // namespace repoplugins
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
+#endif
---
+++ src/nava/core/repoplugins/utils.cc
@@ -0,0 +1,14 @@
+#include "nava/core/repoplugins/utils.h"
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace repoplugins {
+Utils::Utils() {}
+
+Utils::~Utils() {}
+
+}  // namespace repoplugins
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
